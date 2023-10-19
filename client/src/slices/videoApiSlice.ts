7167e68b5f6ddb3ae722431b69b013ed723f1f78
Patrick Boynton <jpboynton3@gmail.com>
2023-10-19 11:09:12 -0400

    Changed logic around query.

--- client/src/slices/videoApiSlice.ts
+++ client/src/slices/videoApiSlice.ts
@@ -4,18 +4,23 @@ import { IVideo } from '../interfaces/interfaces'
 export const videoApiSlice = apiSlice.injectEndpoints({
   endpoints: builder => ({
     getVideos: builder.query<IVideo[], any>({
-      query: ({ keyword = '' }) => {
-        let url = '/';
+      query: keyword => {
+        let url = '/'
 
-        if (keyword) {
-          url = `/search?title=${keyword}`;
+        if (
+          keyword !== '' ||
+          keyword !== undefined ||
+          keyword !== typeof Object
+        ) {
+          url = `/search?title=${keyword}`
         }
+
         return {
           url,
           params: {
             keyword,
           },
-        };
+        }
       },
     }),
     getRandomVideo: builder.query<IVideo, void>({
@@ -24,7 +29,7 @@ export const videoApiSlice = apiSlice.injectEndpoints({
       }),
     }),
     getPlayedVideos: builder.query<IVideo[], any>({
-      query: ({ keyword = ''}) => ({
+      query: keyword => ({
         url: '/played',
         params: {
           keyword,
@@ -34,15 +39,15 @@ export const videoApiSlice = apiSlice.injectEndpoints({
     deletePlayedVideos: builder.mutation({
       query: () => ({
         url: '/random',
-        method: 'DELETE'
-      })
+        method: 'DELETE',
+      }),
     }),
     updateVideo: builder.mutation({
       query: ({ _id, updatedData }) => ({
         url: `/edit/${_id}`,
         method: 'PUT',
-        body: updatedData
-      })
+        body: updatedData,
+      }),
     }),
     uploadVideo: builder.mutation<string, FormData>({
       query: (formData: FormData) => ({
