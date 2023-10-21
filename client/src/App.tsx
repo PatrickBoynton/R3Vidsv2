0c7db5dc29843fec7a3a109c5f71db7154025188
Patrick Boynton <jpboynton3@gmail.com>
2023-10-21 12:31:09 -0400

    Added a way of toggling the detail screen

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,14 +3,20 @@ import { Button } from '@mui/material'
 import RandomScreen from './screens/RandomScreen'
 import TutorialScreen from './screens/TutorialScreen'
 import { useState } from 'react'
+import TutorialDetailScreen from './screens/TutorialDetailScreen'
 
 const App = () => {
   const [isTutorial, setIsTutorial] = useState(false)
   return (
     <>
-      <Button onClick={() => setIsTutorial(!isTutorial)}>Tutorial Toggle</Button>
-      {!isTutorial ? <RandomScreen /> :
-        <TutorialScreen />}
+      <Button onClick={() => setIsTutorial(!isTutorial)}>
+        Tutorial Toggle
+      </Button>
+      {!isTutorial ? (
+        <RandomScreen />
+      ) : <TutorialScreen /> ? (
+        <TutorialDetailScreen />
+      ) : null}
     </>
   )
 }
