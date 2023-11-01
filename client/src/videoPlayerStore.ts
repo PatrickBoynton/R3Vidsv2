8bc9bec3799a0667ea4f01e89b3d87eee66716a2
Patrick Boynton <jpboynton3@gmail.com>
2023-10-31 22:26:06 -0400

    Continued moving video player related state here.

--- client/src/videoPlayerStore.ts
+++ client/src/videoPlayerStore.ts
@@ -5,20 +5,39 @@ interface VideoPlayerState {
   playing: boolean
   url: string
   volume: number
+  muted: boolean
+  fullScreen: boolean
   currentIndex: number
+  progress: number
   title: string
   description: string
   metaData: Metadata
   currentPlayTime: number
   playCount: number
   lastPlayed: Date
+  setPlaying: () => void
+  setUrl: (url: string) => void
+  setVolume: (volume: number) => void
+  setMuted: (muted: boolean) => void
+  setFullScreen: () => void
+  setCurrentIndex: (currentIndex: number) => void
+  setProgress: (progress: number) => void
+  setTitle: (title: string) => void
+  setDescription: (description: string) => void
+  setMetaData: (metaData: Metadata) => void
+  setCurrentPlayTime: (currentPlayTime: number) => void
+  setPlayCount: (playCount: number) => void
+  setLastPlayed: (lastPlayed: Date) => void
 }
 
 const useVideoPlayerStore = create<VideoPlayerState>(set => ({
   playing: false,
   url: '',
   volume: 0,
+  muted: false,
+  fullScreen: false,
   currentIndex: 0,
+  progress: 0,
   title: '',
   description: '',
   metaData: { duration: 0 },
@@ -34,8 +53,17 @@ const useVideoPlayerStore = create<VideoPlayerState>(set => ({
   setVolume: (volume: number) => {
     set(() => ({ volume }))
   },
+  setMuted: (muted: boolean) => {
+    set(() => ({ muted }))
+  },
+  setFullScreen: () => {
+    set(state => ({ fullScreen: !state.fullScreen }))
+  },
   setCurrentIndex: (currentIndex: number) => {
     set(() => ({ currentIndex }))
+  },
+  setProgress: (progress: number) => {
+    set(() => ({ progress }))
   },
   setTitle: (title: string) => {
     set(() => ({ title }))
