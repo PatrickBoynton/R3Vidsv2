c0450d52f6485ba7a23c67ce70620dbdacd95df1
Patrick Boynton <youremail@yourdomain.com>
2023-12-27 12:09:31 -0500

    Removed the old React client.

--- client/src/utils/constants.ts
+++
@@ -1,3 +0,0 @@
-export const BASE_URL: string = '/api/videos'
-
-export const RANDOM_URL: string = BASE_URL + '/random'
--- client/src/utils/styles.ts
+++
@@ -1,3 +0,0 @@
-export  const IconStyles = { color: '#a854a8', width: '48px', height: '48px'}
-export const divStyles = { display: 'flex', margin: '15px 0'}
-export const  TextColors = { color: '#ffdb60' }
