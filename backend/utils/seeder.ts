c0036928727f1f53b6ef05a126ef6a33f4b3d866
Patrick Boynton <youremail@yourdomain.com>
2024-01-11 16:37:03 -0500

    Minor refactoring.

--- backend/utils/seeder.ts
+++ backend/utils/seeder.ts
@@ -6,41 +6,43 @@ import { Video } from '../models/videoSchema'
 import { createNewVideo, updateVideoUrl } from './controllerUtils'
 
 export const seeder = async (dir: string) => {
+    const videoCount = await Video.countDocuments()
+    const testVideo = await Video.findOne({ title: 'AA1' })
+    const testUrl = testVideo?.url ? new URL(testVideo?.url as string) : ''
+
+    const testIpPath = new URL(
+        `http://${getIpAddress()}:8000/${testVideo?.title}`
+    )
+
     try {
         const files = fs.readdirSync(dir)
-
         if (files.length === 0) {
             console.info(
                 'No files found. Check to see if the docker container is running.'
             )
             return
         }
-
-        const videoCount = await Video.countDocuments()
-        const testVideo = await Video.findOne({ title: 'AA1' })
-        const testUrl = new URL(testVideo?.url as string)
-        const testIpPath = new URL(
-            `http://${getIpAddress()}:8000/${testVideo?.title}`
-        )
-
-        if (testIpPath.origin === testUrl.origin) {
-            console.info('Ip addresses are  the same.')
-        } else {
-            console.warn(
-                'Ip address has changed, please update the agent file.'
-            )
-            console.info(
-                `Go to http://${getIpAddress()}:8000/ to reach the application on your device.`
-            )
+        if (testUrl) {
+            if (testIpPath.origin === testUrl.origin) {
+                console.info('Ip addresses are  the same.')
+            } else {
+                console.warn(
+                    'Ip address has changed, please update the agent file.'
+                )
+                console.info(
+                    `Go to http://${getIpAddress()}:5173/ to reach the application on your device.`
+                )
+            }
         }
-
         for (const file of files) {
+            const title = file.replace(/\.[^/.]+$/, '')
+            const existingVideo = await Video.findOne({ title })
             const filePath = path.join(dir, file)
-
             if (isVideoFile(file)) {
-                const title = file.replace(/\.[^/.]+$/, '')
-                const existingVideo = await Video.findOne({ title })
-                const url = new URL(existingVideo?.url as string)
+                const url = new URL(
+                    (existingVideo?.url as string) ||
+                        `http://${getIpAddress()}:8000/${file}`
+                )
                 const ipPath = new URL(`http://${getIpAddress()}:8000/${file}`)
 
                 if (!existingVideo) {
@@ -49,17 +51,14 @@ export const seeder = async (dir: string) => {
 
                 if (url.origin !== ipPath.origin) {
                     await updateVideoUrl(title, file)
-                    console.warn('Please update the agent file.')
                 }
             }
         }
-
         if (files.length === videoCount) {
             console.info('Video count:', videoCount)
             console.info(
                 'Number of video files in the directory matches the video count in the collection. No new videos to add.'
             )
-            return
         }
         console.info('Video count:', videoCount)
         console.info('Finished.')
