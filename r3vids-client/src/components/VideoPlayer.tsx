0b59a0b53521ceb9b6ee2baff67bada41db7d106
Patrick Boynton <youremail@yourdomain.com>
2024-01-16 01:26:27 -0500

    Added the ability to seek to the last played position.

--- r3vids-client/src/components/VideoPlayer.tsx
+++ r3vids-client/src/components/VideoPlayer.tsx
@@ -4,12 +4,24 @@ import { useVideoPropertyStore } from '../stores/videoPropertyStore.ts'
 import { useRef } from 'react'
 
 const VideoPlayer = () => {
-	const { url } = useVideoPropertyStore()
+	const { url, currentPlayTime } = useVideoPropertyStore()
 	const vidRef = useRef<ReactPlayer | null>(null)
 
+	const handleStart = () => {
+		try {
+			vidRef.current?.seekTo(currentPlayTime)
+		} catch (e) {
+			console.error('HandleStart: ', e)
+		}
+	}
 	return (
 		<>
-			<ReactPlayer controls url={url} ref={vidRef} />
+			<ReactPlayer
+				controls
+				url={url}
+				ref={vidRef}
+				onStart={() => handleStart()}
+			/>
 			<Controls vidRef={vidRef} />
 		</>
 	)
