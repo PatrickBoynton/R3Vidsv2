00a8b25984c3fcce651f9d9cda3d6172193239d7
Patrick Boynton <youremail@yourdomain.com>
2024-01-16 01:31:00 -0500

    Added the previous video method. Updated the update video.

--- r3vids-client/src/stores/videoApiStore.ts
+++ r3vids-client/src/stores/videoApiStore.ts
@@ -6,12 +6,14 @@ type VideoState = {
 	videos: Video[] | null
 	playedVideos: Video[] | null
 	randomVideo: Video | null
+	previousVideo: Video | null
 	randomPlayedVideo: Video | null
 	currentIndex: number
 	searchTerm: string
 	getVideos: (searchTerm?: string) => void
 	getPlayedVideos: () => void
 	getRandomVideo: () => void
+	getPreviousVideo: () => void
 	getRandomPlayedVideo: () => void
 	deletePlayedVideos: () => void
 	updateVideo: (video: Partial<Video>) => void
@@ -24,26 +26,44 @@ export const useVideoApiStore = create<VideoState>(set => ({
 	randomPlayedVideo: null,
 	currentIndex: 0,
 	randomVideo: null,
+	previousVideo: null,
 	searchTerm: '',
 	getVideos: async (searchTerm = '') => {
 		const videos = await agent.Videos.list(searchTerm)
-		set({ searchTerm })
-		set({ videos })
+		set(state => ({ ...state, searchTerm }))
+		set(state => ({ ...state, videos }))
 	},
 	getPlayedVideos: async () => {
-		const videos = await agent.Videos.played()
-		set({ playedVideos: videos })
+		const playedVideos = await agent.Videos.played()
+		set(state => ({ ...state, playedVideos }))
 	},
 	getRandomVideo: async () => {
 		const randomVideo = await agent.Videos.random()
-		set({ randomVideo })
+		set(state => ({ ...state, randomVideo }))
+	},
+	getPreviousVideo: async () => {
+		const previousVideo = await agent.Videos.previous()
+		set(state => ({ ...state, previousVideo }))
 	},
 	getRandomPlayedVideo: async () => {
 		const randomPlayedVideo = await agent.Videos.randomPlayed()
-		set({ randomPlayedVideo })
+		set(state => ({ ...state, randomPlayedVideo }))
 	},
-	updateVideo: (video: Partial<Video>) => {
+	updateVideo: async (video: Partial<Video>) => {
 		agent.Videos.update(video)
+		// Updates the regular list.
+		// Gets and sets the newly updated played list to state.
+		const playedVideos = await agent.Videos.played()
+		if (playedVideos.length > 0) set({ playedVideos })
+
+		set(state => ({
+			...state,
+			videos: state.videos?.map(v =>
+				// If the updated video is equal to the passed in,
+				// spread it to the state, if it is the only one,then add it as is.
+				v._id === video._id ? { ...v, ...video } : v,
+			),
+		}))
 	},
 	deletePlayedVideos: async () => {
 		set({ playedVideos: [] })
