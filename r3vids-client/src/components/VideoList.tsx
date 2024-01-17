4641c9872ce63d7ca44283d01a5a1326957a776c
Patrick Boynton <youremail@yourdomain.com>
2024-01-17 15:55:57 -0500

    Added a tags check.

--- r3vids-client/src/components/VideoList.tsx
+++ r3vids-client/src/components/VideoList.tsx
@@ -2,15 +2,14 @@ import { Grid } from '@mui/material'
 import VideoCard from './VideoCard.tsx'
 import { useVideoApiStore } from '../stores/videoApiStore.ts'
 import FilterSearch from './filterSearch/FilterSearch.tsx'
+import { useTagsStore } from '../stores/tagsStore.ts'
 
 const VideoList = () => {
 	const { videos } = useVideoApiStore()
-
+	const { tags } = useTagsStore()
 	return (
 		<Grid container spacing={1}>
-			<Grid item>
-				<FilterSearch />
-			</Grid>
+			<Grid item>{tags && <FilterSearch />}</Grid>
 			{videos
 				?.sort((a, b) => b.playCount - a.playCount)
 				.map(video => (
