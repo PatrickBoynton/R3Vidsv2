3dd9e95c7bec4531356d7460d625625593df6696
Patrick Boynton <youremail@yourdomain.com>
2024-02-02 12:13:38 -0500

    Updated the random video method. Removed the useEffect.

--- r3vids-client/src/components/controls/playerButtons/RandomButton.tsx
+++ r3vids-client/src/components/controls/playerButtons/RandomButton.tsx
@@ -2,17 +2,12 @@ import ControlIcon from '../ControlIcon.tsx'
 import { useVideoApiStore } from '../../../stores/videoApiStore.ts'
 import ShuffleIcon from '@mui/icons-material/Shuffle'
 import { iconStyles } from '../../../styles.ts'
-import { useEffect } from 'react'
 
 const RandomButton = () => {
-	const { getRandomVideo, randomVideo, getPlayedVideos } = useVideoApiStore()
+	const { getRandomVideo } = useVideoApiStore()
 	const handleGetRandomVideo = () => {
 		getRandomVideo()
-		getPlayedVideos()
 	}
-	useEffect(() => {
-		getPlayedVideos()
-	}, [randomVideo, getPlayedVideos])
 	return (
 		<ControlIcon
 			onClick={handleGetRandomVideo}
